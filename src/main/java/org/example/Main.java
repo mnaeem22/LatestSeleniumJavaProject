092479cb37caf6ab6f819d0e6f6e81f709ee5936
Muhammad Naeem <mnaeem@connecture.com>
2025-12-07 00:40:06 -0800

    local change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        //Changes from Local
+        //Changes from Local for github
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
