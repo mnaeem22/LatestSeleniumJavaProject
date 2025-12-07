ad3f073f67b71a456c747431d3815193b2c7ac29
Muhammad Naeem <mnaeem@gmail.com>
2025-12-07 00:42:12 -0800

    Remove local changes comment from Main.java
    Removed unnecessary comment about local changes.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        //Changes from Local for github
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
